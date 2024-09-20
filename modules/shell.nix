56595109939d309b869ee7c07a728c060e51c155
Alix Macdonald <alixmacdonald10@googlemail.com>
2024-09-20 09:34:42 +0100

    feat: updated tmux config

--- modules/shell.nix
+++ modules/shell.nix
@@ -71,7 +71,12 @@
     mouse = true;
     extraConfig = ''
       set-option -sa terminal-overrides ",xterm*:Tc"
-      
+
+      set -g status-position top
+
+      unbind r
+      bind r source-file ~/.config/tmux/tmux.conf
+
       set -g base-index 1
       set -g pane-base-index 1
       set-window-option -g pane-base-index 1
@@ -83,6 +88,23 @@
       
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
+      
+      set -g @catppuccin_window_left_separator "█"
+      set -g @catppuccin_window_right_separator "█ "
+      set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_middle_separator "  █"
+
+      set -g @catppuccin_window_default_fill "number"
+
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#{pane_current_path}"
+
+      set -g @catppuccin_status_modules_right "application session date_time"
+      set -g @catppuccin_status_left_separator  ""
+      set -g @catppuccin_status_right_separator " "
+      set -g @catppuccin_status_fill "all"
+      set -g @catppuccin_status_connect_separator "yes"
+
     '';
     plugins = with pkgs; [
       {
