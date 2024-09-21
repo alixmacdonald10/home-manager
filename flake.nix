e986267a711d148d390910dfb70b95aca0172473
Alix Macdonald <alixmacdonald10@googlemail.com>
2024-09-21 09:50:09 +0100

    feat: removed flake.nix as this is not bespoke to the system

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Home Manager configuration of amac";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        config.allowUnfree = true; 
+        inherit system;
+        overlays = [
+          neovim-nightly-overlay.overlays.default
+        ];
+      };
+    in {
+      homeConfigurations."amac" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+          ./modules/general.nix
+      	  ./modules/shell.nix
+      	  ./modules/dev.nix
+	        ./modules/bsc.nix
+	      ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
