717d978552102e01d0288579c1f90d0a73692bf2
Alix Macdonald <amacdonald@bscglobal.com>
2024-11-05 17:21:32 +0000

    feat: added flameshot and set as default screen shoter

--- modules/general.nix
+++ modules/general.nix
@@ -4,13 +4,23 @@
   home.packages = [
     pkgs.neofetch
     pkgs.firefox
+    pkgs.flameshot
   ];
-
-  home.file = {
-
-  };
 
   home.sessionVariables = {
     BROWSER = "${pkgs.firefox}/bin/firefox";
+  };
+
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Flameshot HM";
+      command = "sh -c 'QT_QPA_PLATFORM=wayland ${pkgs.flameshot}/bin/flameshot gui'";
+      binding = "Print";
+    };      
   };
 }
